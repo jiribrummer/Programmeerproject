45570e9071a4f74b8d15ee3e16beca86f0b02417
jiribrummer <jiribrummer@gmail.com>
2015-01-16 20:20:34 +0100

    Commit 16-1. All data plotted in line graph. Now working.

--- line.js
+++ line.js
@@ -24,12 +24,16 @@ var yAxis = d3.svg.axis()
 var line = d3.svg.line()
     .x(function(d) { return x(d.jaar); })
     .y(function(d) { return y(d.aantal); });
+	
 
 var svg = d3.select("#linegraph").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+var color = d3.scale.category10();
+
 // End of copied code.
 
 
@@ -37,43 +41,95 @@ var svg = d3.select("#linegraph").append("svg")
 
 console.log('start');
 
+// Load data en wait to execute main function until all data is loaded
 queue()
-    .defer(d3.tsv, 'Data/overviewData.txt') // 
-    .defer(d3.tsv, 'Data/testdata.txt') // 
-	.defer(d3.tsv, 'Data/emolabels.txt') // 
-    .await(main); // 
+    .defer(d3.tsv, 'Data/overviewData.txt') // Data with background information
+    .defer(d3.tsv, 'Data/alldata.txt') // Data with all texts
+	.defer(d3.tsv, 'Data/emolabels.txt') // All emotion labels
+    .await(main); // Wait for all data sets to be loaded to execute main function
 
+// Shape overview data in correct format
 function prepareData(overviewdata) {
 	overviewdata.forEach(function(d) {
-		d.aantal = +d.aantal;
-		d.jaar = +d.jaar;	
+		d.aantal = +d.aantal; // Make integers of string
+		d.jaar = +d.jaar;	// Make integers of string
 })};
 
-function createEmptyJson(overviewdata) {
-	var data = []
+// Function to create a json file with the name of an given emotion
+function addToJson(overviewdata, varname, dataToAdd) {
+	//console.log(varname)
+		dataToAdd[varname] = [] // Add name of given emotion
+	//console.log(dataToAdd)
 	overviewdata.forEach(function(d) {
-		data.push({'jaar' : d.jaar, 'aantal' : 0})
+		dataToAdd[varname].push({'jaar' : d.jaar, 'aantal' : 0}) // Set all counts 0 for every year
 	})
-	return data
+	//console.log(dataToAdd)
+	return dataToAdd
+}
+
+function plotLine(plotdata, i) {
+
+  svg.append("path")
+      .datum(plotdata)
+      .attr("class", "line")
+      .attr("d", line)
+	  .style("opacity", 0.4);
+	  
+	  
+	  
+
+    svg.selectAll("dot")
+        .data(plotdata)
+    .enter().append("circle")
+        .attr("r", 5)
+        .attr("cx", function(d) { return x(d.jaar); })
+        .attr("cy", function(d) { return y(d.aantal); })
+		.style("fill", "steelblue");
+
+                
+		
+	
+}
+
+function calculateEmotions(overviewdata, alldata, emolabels, emolevel) {
+	totalEmotions = countTotalEmotions(overviewdata, alldata, emolevel)
+	//console.log(totalEmotions)
+	var emoData = {} // Make empty JSON data to add emotions
+	//console.log(emoData)
+	for (var i in emolabels) { 
+	console.log(emolabels[i].emolabels)
+	
+		specEmotion = countSpecifiedEmotion(overviewdata, alldata, emolabels[i].emolabels, emolevel, emoData);
+		//console.log(specEmotion)
+	
+		plotdata = calculateRelativeFrequency(specEmotion, totalEmotions, emolabels[i].emolabels)
+		//console.log(plotdata[emolabels[i]])
+		
+		x.domain(d3.extent(plotdata[emolabels[i].emolabels], function(d) { return d.jaar; }));
+		y.domain(d3.extent(plotdata[emolabels[i].emolabels], function(d) { return d.aantal; }));
+		
+		plotLine(plotdata[emolabels[i].emolabels], i)
+	}
+	//console.log(plotdata)
+	return plotdata
 }
 
 
-
 // Main function
-function main(error, overviewdata, testdata, emolabels) {
-	
+function main(error, overviewdata, alldata, emolabels) {
+	console.log(emolabels)
 	prepareData(overviewdata)
-	//console.log(createEmptyJson(overviewdata, 'Blijdschap'))
+	//console.log(addToJson(overviewdata, 'Woede'))
 	console.log("============")
 	
-	totalEmotions = countTotalEmotions(overviewdata, testdata, 'Lichaamsdeel')
-	console.log(totalEmotions)
-	specEmotion = countSpecifiedEmotion(overviewdata, testdata, 'Angst', 'Lichaamsdeel');
-	
-	plotdata = calculateRelativeFrequency(specEmotion, totalEmotions)
+	var labels = emolabels
+	console.log(emolabels)
+	calculateEmotions(overviewdata, alldata, labels, 'Emotie')
 
-	x.domain(d3.extent(plotdata, function(d) { return d.jaar; }));
-	y.domain(d3.extent(plotdata, function(d) { return d.aantal; }));
+	
+	
+	
+
 
   svg.append("g")
       .attr("class", "x axis")
@@ -96,21 +152,17 @@ function main(error, overviewdata, testdata, emolabels) {
       .style("text-anchor", "end")
       .text("Frequency (%)");
 
-  svg.append("path")
-      .datum(plotdata)
-      .attr("class", "line")
-      .attr("d", line);
-	  
-	  console.log(overviewdata)
-	  console.log('finish')
+	
+	
+	console.log('finish')
 }
 
 // Calculate frequency of emotions to plot
-function countSpecifiedEmotion(overviewdata, testdata, emolabel, emolevel) {
-	console.log('----==============--------------')
-	jsondata = createEmptyJson(overviewdata)
-	console.log(jsondata)
-	testdata.forEach(function(d) {
+function countSpecifiedEmotion(overviewdata, alldata, emolabel, emolevel, dataToAdd) {
+	//console.log('----==============--------------')
+	jsondata = addToJson(overviewdata, emolabel, dataToAdd)
+	//console.log(jsondata)
+	alldata.forEach(function(d) {
 		//console.log('in forEach');
 		//console.log(d.emolabel)
 		if(d.emolabel == emolabel && d.emolevel == emolevel) {
@@ -123,64 +175,69 @@ function countSpecifiedEmotion(overviewdata, testdata, emolabel, emolevel) {
 				//console.log(e.id)
 				if(e.id == d.id.substr(0,13)) {
 					//console.log('------------')
-					for (var key in jsondata) {
-						//console.log(jsondata[key].jaar)
-						if (jsondata[key].jaar == e.jaar) {
-							jsondata[key].aantal = jsondata[key].aantal + 1
+					for (var key in jsondata[emolabel]) {
+						//console.log(jsondata[emolabel][key].jaar)
+						if (jsondata[emolabel][key].jaar == e.jaar) {
+							jsondata[emolabel][key].aantal = jsondata[emolabel][key].aantal + 1
 						}
 					}
 				}})
 		}
 		});
-	console.log(overviewdata);
-	console.log(jsondata);
-	console.log('End calcEm');
+	//console.log(overviewdata);
+	//console.log(jsondata);
+	//console.log('End calcEm');
 	return jsondata
 }
 
-function countTotalEmotions(overviewdata, testdata, emolevel) {
-	console.log('----=====++++=======--------------')
-	jsondata = createEmptyJson(overviewdata)
-	console.log(jsondata)
-	testdata.forEach(function(d) {
+function countTotalEmotions(overviewdata, alldata, emolevel) {
+	//console.log('----=====++++=======--------------')
+	//console.log(jsondataTotal)
+	var jsondataTotal = {}  
+	console.log('')
+	addToJson(overviewdata, 'total', jsondataTotal)
+	console.log(jsondataTotal)
+	alldata.forEach(function(d) {
 		//console.log('in forEach');
 		//console.log(d.emolabel)
 		if(d.emolevel == emolevel) {
 			
-			console.log('in if');
-			console.log(jsondata)
+			//console.log('in if');
+			//console.log(jsondata)
 			//console.log(d.id.substr(0,13))
 			//console.log('check')
 			overviewdata.forEach(function(e) {
 				//console.log(e.id)
 				if(e.id == d.id.substr(0,13)) {
-					console.log('------------')
-					for (var key in jsondata) {
-						//console.log(jsondata[key].jaar)
-						if (jsondata[key].jaar == e.jaar) {
-							jsondata[key].aantal = jsondata[key].aantal + 1
+					//console.log('------------')
+					for (var key in jsondataTotal['total']) {
+						//console.log(jsondata[emolabel][key].jaar)
+						if (jsondataTotal['total'][key].jaar == e.jaar) {
+							jsondataTotal['total'][key].aantal = jsondataTotal['total'][key].aantal + 1
 						}
 					}
 				}})
 		}
 		});
 	console.log(overviewdata);
-	console.log(jsondata);
+	console.log(jsondataTotal);
 	console.log('End calcEm');
-	return jsondata
+	return jsondataTotal
 }
 
-function calculateRelativeFrequency(specifiedEmotion, totalEmotion) {
-	
-	for (var i in specifiedEmotion) {
-		console.log('=-=-=-=-=-=-=-=-=')
-		console.log(specifiedEmotion[i].aantal)
-		console.log(totalEmotion[i].aantal)
-		if (specifiedEmotion[i].aantal != 0) {
-			specifiedEmotion[i].aantal = (specifiedEmotion[i].aantal / totalEmotion[i].aantal * 100)
+function calculateRelativeFrequency(specifiedEmotion, totalEmotion, labels) {
+	console.log(specifiedEmotion)
+	console.log(specifiedEmotion)
+	for (var i in specifiedEmotion[labels]) {
+		//console.log('=-=-=-=-=-=-=-=-=')
+		//console.log(specifiedEmotion[labels][i].aantal)
+		//console.log(totalEmotion['total'][i].aantal)
+		if (specifiedEmotion[labels][i].aantal != 0) {
+			specifiedEmotion[labels][i].aantal = (specifiedEmotion[labels][i].aantal / totalEmotion['total'][i].aantal * 100)
 		}
-		console.log(specifiedEmotion[i].aantal)
-		console.log('=-=-=-=-=-=-=-=-=')
+		//console.log(specifiedEmotion[i].aantal)
+		//console.log('=-=-=-=-=-=-=-=-=')
 	}
+	console.log(specifiedEmotion)
 	return specifiedEmotion
 }
